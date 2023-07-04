4eaf145677f2a0ab20e0d99d46895317de6a666d
yazbabamyaz <yazilimagirisim@gmail.com>
2023-07-04 17:17:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Func<double, double> square = Square;
+        Console.WriteLine($"Karesi = {square(4)}");
+
+        Func<double, double> cubic = Cubic;
+        Console.WriteLine($"Küpü = {cubic(4)}");
+
+        Func<string> goster= Show;
+        goster.Invoke();
+    }
+
+    static double Square(double number)=>Math.Pow(number,2);
+    static double Cubic(double number) => Math.Pow(number, 3);
+
+    static string Show()
+    { Console.WriteLine("Test Message..."); 
+        return "";
+    }
+}
